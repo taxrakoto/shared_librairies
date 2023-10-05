2bce3bcd6a79a3c34ddfbb25f39d522ff1755b90
Tahina Rakotomaharo <tahinarakotomaharo@gmail.com>
2023-10-05 13:05:26 +0000

    Update DockerDeployPipelineV2.groovy: agent none

--- vars/DockerDeployPipelineV2.groovy
+++ vars/DockerDeployPipelineV2.groovy
@@ -12,7 +12,7 @@ def call(body) {
     
     /********************** Begining declarative Pipeline **********************************/
     pipeline {
-    agent any   
+    agent none   
           
    
     stages {
