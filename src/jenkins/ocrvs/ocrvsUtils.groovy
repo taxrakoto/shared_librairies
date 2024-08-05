f1e0528c78ec787530a71dc5da6813895aae5e44
Tahina Rakotomaharo <tahina@PRODIGY7>
2024-08-05 17:36:06 +0300

    debugging map

--- src/jenkins/ocrvs/ocrvsUtils.groovy
+++ src/jenkins/ocrvs/ocrvsUtils.groovy
@@ -21,13 +21,13 @@ def get_secret(String ENV, String SECRET_NAME, String KEY_NAME){
         // DEBUGGING
         sh """
             echo "properties file" && cat .properties"
-            echo " secret name is SECRET_NAME "
-            echo " secret value is {propertiesMap[SECRET_NAME]} "
+            echo "secret name is SECRET_NAME "
+            echo "secret value is {propertiesMap[SECRET_NAME]} "
             echo "DEBUG secret  = {env.SECRET_NAME}"
             echo "DEBUG key = {env.KEY_NAME}"
         """
         // END OF DEBUGGING
-        /*
+        
         // check if the secret is empty
         if (!env.SECRET_NAME) {
             error("Secret is empty. Make sure you have added the secret to the Jenkins credentials.")
@@ -42,7 +42,7 @@ def get_secret(String ENV, String SECRET_NAME, String KEY_NAME){
      
         // return the result
         return encodedEncryptedSecret
-        */    
+            
      }  
 }
 /****************************************************************************************************/
