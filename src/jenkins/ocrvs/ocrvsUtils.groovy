106d7a4528808eedb81eab1dc948a620384ec198
Tahina Rakotomaharo <tahina@PRODIGY7>
2024-08-05 17:25:16 +0300

    debugging map

--- src/jenkins/ocrvs/ocrvsUtils.groovy
+++ src/jenkins/ocrvs/ocrvsUtils.groovy
@@ -13,22 +13,21 @@ def get_secret(String ENV, String SECRET_NAME, String KEY_NAME){
     withCredentials([file(credentialsId: ENV, variable: 'ENV_SECRET_FILE')]) {
         // read the secret file into a map
         sh 'touch .properties && cat ${ENV_SECRET_FILE} > .properties'
-        sh ' cat .properties'
         script {
             def props = readProperties file :'.properties'
             props.each { key, value -> propertiesMap[key] = value }
         }
-        /*
+        
         // DEBUGGING
         sh """
             echo "properties file" && cat .properties"
-            echo " secret name is $SECRET_NAME "
-            echo " secret value is ${propertiesMap[$SECRET_NAME]} "
-            echo "DEBUG secret  = ${env.SECRET_NAME}"
-            echo "DEBUG key = ${env.KEY_NAME}"
+            echo " secret name is SECRET_NAME "
+            echo " secret value is {propertiesMap[SECRET_NAME]} "
+            echo "DEBUG secret  = {env.SECRET_NAME}"
+            echo "DEBUG key = {env.KEY_NAME}"
         """
         // END OF DEBUGGING
-        
+        /*
         // check if the secret is empty
         if (!env.SECRET_NAME) {
             error("Secret is empty. Make sure you have added the secret to the Jenkins credentials.")
